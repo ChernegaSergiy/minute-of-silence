20736330ab8d62c3ed2b3809bc11e6ceccb059ee
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-05 00:12:37 +0300

    fix: Simplify autostart logic and resolve duplicate argument issues in Snap

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,11 +27,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
-            if std::env::var("SNAP").is_ok() {
-                Some(vec!["minute-of-silence", "--hidden"])
-            } else {
-                Some(vec!["--hidden"])
-            },
+            Some(vec!["--hidden"]),
         ))
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_shell::init())
