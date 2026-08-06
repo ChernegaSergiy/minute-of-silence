8d6d75ca8455c169693970d339c9968035b74e67
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-06 20:50:34 +0300

    style: Remove Byte Order Mark (BOM) from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-﻿import { Suspense, lazy, useCallback, useEffect, useRef, useState } from "react";
+import { Suspense, lazy, useCallback, useEffect, useRef, useState } from "react";
 import {
   Button,
   FluentProvider,
